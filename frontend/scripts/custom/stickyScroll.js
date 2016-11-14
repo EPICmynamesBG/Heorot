02004c2259c2ad109e562e1f5b58bbd0fb5f3b2f
Brandon Groff <mynamesbg@gmail.com>
2016-11-14 13:16:42 -0500

    Remove console log

--- frontend/scripts/custom/stickyScroll.js
+++ frontend/scripts/custom/stickyScroll.js
@@ -3,7 +3,6 @@ var spacer = $("#sticky-spacer");
 var wrapper = $('#scroll-wrapper');
 
 wrapper.on("scroll", function (e) {
-    console.log(wrapper.scrollTop());
     if (wrapper.scrollTop() > 189 && $(window).width() > 992) {
         obj.addClass("stuck");
         spacer.addClass('active');
