ed9da2b4e7a20c8442d7e56a6240ad04cea564cf
Brandon Groff <mynamesbg@gmail.com>
2016-11-14 12:59:34 -0500

    Add filtered count with animation. Added some beers to DB. Updated search sticky search fields. Add icon

--- frontend/scripts/custom/stickyScroll.js
+++ frontend/scripts/custom/stickyScroll.js
@@ -3,7 +3,8 @@ var spacer = $("#sticky-spacer");
 var wrapper = $('#scroll-wrapper');
 
 wrapper.on("scroll", function (e) {
-    if (wrapper.scrollTop() > 144 && $(window).width() > 992) {
+    console.log(wrapper.scrollTop());
+    if (wrapper.scrollTop() > 189 && $(window).width() > 992) {
         obj.addClass("stuck");
         spacer.addClass('active');
     } else {
---
+++ frontend/scripts/directives/animateNumberOnChange.js
@@ -0,0 +1,51 @@
+app.directive('animateNumberOnChange', function ($timeout) {
+  var minDuration = 100;
+  var maxDuration = 900;
+  return {
+    restrict: 'A',
+    scope: {
+      animateNumberOnChange: '='
+    },
+    transclude: false,
+    link: function (scope, element, attrs) {
+
+      function init() {
+        //set initial value
+        element.text(scope.animateOnChange);
+
+        scope.$watch('animateNumberOnChange', function (newVal, oldVal) {
+          if (newVal != oldVal) {
+
+            var diff = Math.abs(oldVal - newVal);
+            var duration = 20 * diff;
+
+            if (duration < minDuration) {
+              duration = 100;
+            }
+            if (duration > maxDuration) {
+              duration = 1000;
+            }
+
+            element.text(oldVal);
+            //halt prior animation first, then apply
+            element.stop(true, true, true)
+              .prop('Counter', oldVal)
+              .animate({
+                Counter: newVal
+              }, {
+                duration: duration,
+                easing: 'swing',
+                step: function (now) {
+                  element.text(Math.ceil(now));
+                }
+              });
+          }
+        });
+      };
+
+      //timeout to allow for initial variable calculation
+      $timeout(init, false);
+
+    }
+  }
+});
